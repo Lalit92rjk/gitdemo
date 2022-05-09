eac26e93df653b5734f0d52721148108e427de07
Lalit92rjk <lalit92rajak@gmail.com>
2022-05-09 12:07:26 +0530

    another task

---
+++ task3/task4.js
@@ -0,0 +1,9 @@
+// making item 3 in green color
+items[2].style.backgroundColor =  'green';
+
+// making color font  bold
+
+for (var i = 0; i<items.length;i++){ items[i].style.fontWeight =  'bold'
+}
+
+for(var i = 0; i<items.length;i++){items[i].style.backgroundColor = '#f4f4f4';}
